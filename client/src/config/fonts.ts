d1739391748d718d3065129e5db49e0db3e3a544
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2026-01-08 11:59:21 +0200

    Initializing Front-End Structure Colors & Fonts...

---
+++ client/src/config/fonts.ts
@@ -0,0 +1,13 @@
+import { Space_Grotesk, Plus_Jakarta_Sans } from "next/font/google";
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+export const plusJakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
