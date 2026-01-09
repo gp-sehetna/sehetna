d96cee33834ff7c4817afc6fb87bfce259701b8e
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2026-01-09 11:29:09 +0200

    Handling pages

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
 import {plusJakarta, spaceGrotesk} from "@/src/config/fonts"
+import AntdProvider from "../providers/AantDProvider";
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -8,7 +10,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       {/* Use plusJakarta as default body font */}
       <body className={plusJakarta.className}>
         {/* Use spaceGrotesk class only on headings */}
-        <div className={spaceGrotesk.className}>{children}</div>
+        <div className={spaceGrotesk.className}>
+          <AntdRegistry>
+            <AntdProvider>{children}</AntdProvider>
+          </AntdRegistry>
+        </div>
       </body>
     </html>
   );
