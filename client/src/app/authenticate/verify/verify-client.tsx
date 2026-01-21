d77dfee10cbc136d126b8b66e5e468792a8d15cb
Mohamed Hussien <b.moody2004@gmail.com>
2026-01-21 20:53:39 +0200

    Refactor verification page: move logic to VerifyClient component and simplify Page component with Suspense.
    FIX FOR
    ```useSearchParams() should be wrapped in a suspense boundary at page "/authenticate/verify". Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout```

---
+++ client/src/app/authenticate/verify/verify-client.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import BaseAuthentication from "@/components/ui/Authentication/BaseAuthentication";
+import WideButton from "@/components/ui/WideButton";
+import { Button } from "@/components/ui/shadcn/button";
+import {
+	InputOTP,
+	InputOTPGroup,
+	InputOTPSeparator,
+	InputOTPSlot,
+} from "@/components/ui/shadcn/input-otp";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const purposes = new Set(["email_verification", "password_reset"]);
+
+export default function VerifyClient() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const requiredParam = searchParams.get("purpose");
+
+  useEffect(() => {
+    if (!requiredParam || !purposes.has(requiredParam)) router.replace("/authenticate/login");
+  }, [requiredParam, router]);
+
+  const title = <h3>Check your inbox</h3>;
+  const subtitle = (
+    <p className="text-md text-neutral-600">Mail could be flagged as spam</p>
+  );
+
+  return (
+    <BaseAuthentication className="md:items-center" title={title} subtitle={subtitle}>
+      <p className="text-center text-sm">
+        We&apos;ve sent you a passcode.
+        <br /> Please check your inbox at a******************@g****.com.
+      </p>
+
+      <InputOTP className="flex justify-center" maxLength={6}>
+        {[0, 1, 2].map((i) => (
+          <InputOTPGroup key={i}>
+            <InputOTPSlot index={i} />
+          </InputOTPGroup>
+        ))}
+
+        <InputOTPSeparator />
+
+        {[3, 4, 5].map((i) => (
+          <InputOTPGroup key={i}>
+            <InputOTPSlot index={i} />
+          </InputOTPGroup>
+        ))}
+      </InputOTP>
+
+      <WideButton variant="black">Continue</WideButton>
+      <Button variant="ghost">Resend Code</Button>
+    </BaseAuthentication>
+  );
+}
