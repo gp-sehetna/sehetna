7e2372bbbfab9dff2fc05850c04779ca23cff17c
Marwan Abdullah <meroosedo011@gmail.com>
2026-01-29 19:07:05 +0200

    .

--- client/src/db/connection.db.ts
+++ client/src/db/connection.db.ts
@@ -1,18 +1,23 @@
 import {connect} from 'mongoose'
-
-export const connectDB = async()=>{
+import {config} from 'dotenv';
+config();
+const connectDB = async()=>{
     try {
-        const result =  await connect(process.env.URI as string);
-
-        console.log("DB connected succcessfully!");
-    } catch (error) {
+        console.log(process.env.CONNECTION_STRING);
         
+        const result =  await connect(process.env.CONNECTION_STRING as string);
+        
+        console.log({models : result.models});
+        console.log("DB connected succcessfully!" );
+    } catch (error) {
+        console.log("Fail to connect on DB" , error);
     }
 
     
 } 
 
 
+export default connectDB;
 
 
-
+await connectDB();
