144ed2400eb0b2803779693cb482aeb31cf2d743
Abdelrahman Hussien Mohamed Abdallah <abdelrahman.hsharaf@gmail.com>
2026-01-29 03:19:18 +0200

    DatePicker component initialized.

--- client/src/components/ui/map/MapView.tsx
+++ client/src/components/ui/map/MapView.tsx
@@ -21,6 +21,7 @@ import type {
 import CountryPopup from "./CountryPopup"
 import ZoomControls from "./ZoomControls"
 import "maplibre-gl/dist/maplibre-gl.css"
+import { DatePickerWithRange } from "../GlobalControls/DatePicker"
 
 const INITIAL_MAP_CONFIG = {
     longitude: 31.23,
@@ -114,6 +115,7 @@ export default function MapView() {
                 onClick={onMapClick}
                 onLoad={onMapLoad}
             />
+            <DatePickerWithRange className="absolute" />
             <ZoomControls onZoomIn={handleZoomIn} onZoomOut={handleZoomOut} />
         </>
     )
@@ -135,7 +137,7 @@ const zoomToCountry = (country: MapGeoJSONFeature, map: maplibregl.Map, centroid
     // For dubugging
     // console.log("Country: ", country)
     // console.log("BBox: ", bounds)
-    drawBBox(map, bounds)
+    // drawBBox(map, bounds)
 
     map.fitBounds(
         [
@@ -170,64 +172,64 @@ const renderPopup = (popupRef: any, properties: any, centroid: LngLatLike, map: 
 }
 
 //? Debugging bbox boundries
-type BBox = [number, number, number, number] | [number, number, number, number, number, number]
-const bboxToPolygon = (
-    minX: number,
-    minY: number,
-    maxX: number,
-    maxY: number
-): GeoJSON.GeoJSON => ({
-    type: "Feature",
-    geometry: {
-        type: "Polygon",
-        coordinates: [
-            [
-                [minX, minY],
-                [maxX, minY],
-                [maxX, maxY],
-                [minX, maxY],
-                [minX, minY], // close ring
-            ],
-        ],
-    },
-    properties: {},
-})
+// type BBox = [number, number, number, number] | [number, number, number, number, number, number]
+// const bboxToPolygon = (
+//     minX: number,
+//     minY: number,
+//     maxX: number,
+//     maxY: number
+// ): GeoJSON.GeoJSON => ({
+//     type: "Feature",
+//     geometry: {
+//         type: "Polygon",
+//         coordinates: [
+//             [
+//                 [minX, minY],
+//                 [maxX, minY],
+//                 [maxX, maxY],
+//                 [minX, maxY],
+//                 [minX, minY], // close ring
+//             ],
+//         ],
+//     },
+//     properties: {},
+// })
 
-const drawBBox = (map: maplibregl.Map, bbox: BBox) => {
-    const [minX, minY, maxX, maxY] = bbox
-    const feature = bboxToPolygon(minX, minY, maxX, maxY)
+// const drawBBox = (map: maplibregl.Map, bbox: BBox) => {
+//     const [minX, minY, maxX, maxY] = bbox
+//     const feature = bboxToPolygon(minX, minY, maxX, maxY)
 
-    const sourceId = "debug-bbox-source"
-    const lineLayerId = "debug-bbox-line"
-    const fillLayerId = "debug-bbox-fill"
+//     const sourceId = "debug-bbox-source"
+//     const lineLayerId = "debug-bbox-line"
+//     const fillLayerId = "debug-bbox-fill"
 
-    if (map.getSource(sourceId)) {
-        ;(map.getSource(sourceId) as maplibregl.GeoJSONSource).setData(feature)
-        return
-    }
+//     if (map.getSource(sourceId)) {
+//         ;(map.getSource(sourceId) as maplibregl.GeoJSONSource).setData(feature)
+//         return
+//     }
 
-    map.addSource(sourceId, {
-        type: "geojson",
-        data: feature,
-    })
+//     map.addSource(sourceId, {
+//         type: "geojson",
+//         data: feature,
+//     })
 
-    map.addLayer({
-        id: fillLayerId,
-        type: "fill",
-        source: sourceId,
-        paint: {
-            "fill-color": "#ff0000",
-            "fill-opacity": 0.15,
-        },
-    })
+//     map.addLayer({
+//         id: fillLayerId,
+//         type: "fill",
+//         source: sourceId,
+//         paint: {
+//             "fill-color": "#ff0000",
+//             "fill-opacity": 0.15,
+//         },
+//     })
 
-    map.addLayer({
-        id: lineLayerId,
-        type: "line",
-        source: sourceId,
-        paint: {
-            "line-color": "#ff0000",
-            "line-width": 2,
-        },
-    })
-}
+//     map.addLayer({
+//         id: lineLayerId,
+//         type: "line",
+//         source: sourceId,
+//         paint: {
+//             "line-color": "#ff0000",
+//             "line-width": 2,
+//         },
+//     })
+// }
