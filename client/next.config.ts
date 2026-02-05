930694c3e1c7f27f459aab8b0a83528454b7dd98
Abdelrahman Hussien Mohamed Abdallah <abdelrahman.hsharaf@gmail.com>
2026-02-05 18:17:07 +0200

    Update countries.geojson to use id as the `promoteId` and update prettier options for next.config.ts and update redirects

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,22 +4,36 @@ const nextConfig: NextConfig = {
     /* config options here */
     allowedDevOrigins: ["127.0.0.1"],
     async rewrites() {
-    if (process.env.NODE_ENV === "production") return []
-    return [
-      {
-        source: "/ai/:path*",
-        destination: "http://127.0.0.1:8000/:path*",
-      },
-    ]
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.tenor.com",
-      },
-    ],
-  },
+        if (process.env.NODE_ENV === "production") return []
+        return [
+            {
+                source: "/ai/:path*",
+                destination: "http://127.0.0.1:8000/:path*",
+            },
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: "/map/:country/respiratory-disease-rate",
+                destination: "/map/:country",
+                permanent: true,
+            },
+            {
+                source: "/map/respiratory-disease-rate",
+                destination: "/map",
+                permanent: true,
+            },
+        ]
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "media.tenor.com",
+            },
+        ],
+    },
 }
 
 export default nextConfig
