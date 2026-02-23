e5350075bddaca83956238b7177ee4402b0bbaab
Mohamed Hussien <b.moody2004@gmail.com>
2026-02-23 21:10:37 +0200

    fix: color bar component wrong import

--- client/src/components/ui/legend/Legend.tsx
+++ client/src/components/ui/legend/Legend.tsx
@@ -1,5 +1,4 @@
 import { toProperCase, unslugify } from "@/lib/utils"
-import { useThemeStore } from "@/stores/map/use-theme"
 import HorizontalColorbar from "./ColorBar"
 import { LegendItem } from "./LegendItem"
 
@@ -8,11 +7,9 @@ export type LegendProps = {
 }
 
 export default function Legend({ healthOutcome }: LegendProps) {
-    const { theme } = useThemeStore()
-
     return (
         <LegendItem label={toProperCase(unslugify(healthOutcome))} unit="%">
-            <HorizontalColorbar colorScale={theme.colorScale} ticks={6} />
+            <HorizontalColorbar ticks={6} />
         </LegendItem>
     )
 }
