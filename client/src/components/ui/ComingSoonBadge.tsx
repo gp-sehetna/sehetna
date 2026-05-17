7e7ed45aa2d4afb75122ce0857a4459ad37c5335
Abdelrahman Hussien Mohamed Abdallah <abdelrahman.hsharaf@gmail.com>
2026-05-17 18:55:35 +0300

    added 2 services in docker compose

--- client/src/components/ui/ComingSoonBadge.tsx
+++ client/src/components/ui/ComingSoonBadge.tsx
@@ -1,11 +1,11 @@
 export const ComingSoonBadge = () => {
     return (
-        <div className="bg-primary-50/20 ring-primary-200 mb-2 inline-flex items-center gap-2 rounded-full px-6 py-1 ring-1">
+        <div className="bg-success-100/2 ring-success-100/50 mb-2 inline-flex items-center gap-2 rounded-full px-6 py-1 ring-1">
             <div className="flex h-2 w-2">
-                <span className="bg-primary-400 absolute inline-flex h-2 w-2 animate-ping rounded-full opacity-75"></span>
-                <span className="bg-primary-500 relative inline-flex h-2 w-2 rounded-full"></span>
+                <span className="bg-success-100 absolute inline-flex h-2 w-2 animate-ping rounded-full opacity-75"></span>
+                <span className="bg-success-200 relative inline-flex h-2 w-2 rounded-full"></span>
             </div>
-            <span className="text-primary-700 text-sm font-medium">Under Active Development</span>
+            <span className="text-success-300 text-sm font-medium">Under Active Development</span>
         </div>
     )
 }
